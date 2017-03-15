d1182c103223d51447509c154c265750fced6037
Sun-fly <645739625@qq.com>
2017-03-15 09:58:56 +0800

    初始化文件

---
+++ 我的测试/我的测试/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  我的测试
+//
+//  Created by 周亚-Sun on 2017/3/15.
+//  Copyright © 2017年 zhouya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 我的测试/我的测试/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  我的测试
+//
+//  Created by 周亚-Sun on 2017/3/15.
+//  Copyright © 2017年 zhouya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor=[UIColor redColor];
+    NSLog(@"你好啊");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
